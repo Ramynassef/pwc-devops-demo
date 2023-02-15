1c5f08328239113b8a3d846e271381b0a684df20
Ramy Nassef <ramy.nassef@pwc.com>
2023-02-15 16:23:55 +0200

    Depoly ECS ECR Env

--- provider.tf
+++ provider.tf
@@ -1,16 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 
-  assume_role {
-    role_arn = "arn:aws:iam::<AWS_Account_Number>:role/ci-cd-deployment-role"
-  }
+    assume_role {
+      role_arn = var.DEPLOY_ROLE
+    }
 }
